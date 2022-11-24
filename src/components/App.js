e528eaec72e73aac7106cc66ef3b1b8b84d11b48
Carl John Manigos <cmanigos@csyoutsourcing.com>
2022-11-24 10:42:24 +0800

    Async writes in Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import AboutPage from "./about/AboutPage";
 import HomePage from "./home/HomePage";
-
+import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
+import CoursesPage from "./courses/CoursesPage";
+import ManageCousesPage from "./courses/ManageCousesPage";
 const App = () => {
   return (
-    <div className="container-fluid">
-      <Route exact path="/" component={HomePage}></Route>
-      <Route path="/about" component={AboutPage}></Route>
+    <div className="container-fluid mt-5">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/about" component={AboutPage}></Route>
+        <Route path="/courses" component={CoursesPage}></Route>
+        <Route path="/course/:slug" component={ManageCousesPage}></Route>
+        <Route path="/course" component={ManageCousesPage}></Route>
+        <Route component={PageNotFound}></Route>
+      </Switch>
     </div>
   );
 };
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return <h1>Opss! Page not found.</h1>;
+};
+
+export default PageNotFound;
